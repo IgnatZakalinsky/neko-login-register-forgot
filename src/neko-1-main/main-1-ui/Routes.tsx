c63b26d9786e3ff763696b4082b7086786e30e91
Ignat <ai73a@yandex.ru>
2019-12-12 18:24:14 +0300

    fix urls

--- src/neko-1-main/main-1-ui/Routes.tsx
+++ src/neko-1-main/main-1-ui/Routes.tsx
@@ -5,25 +5,22 @@ import RegisterPage from '../../neko-3-register/register-1-ui/RegisterPage';
 import ForgotPage from '../../neko-4-forgot/forgot-1-ui/ForgotPage';
 import NekoPage from "../../neko-6-neko/neko-1-ui/NekoPage";
 
-export const SIGN_IN_PATH = '/sign-in';
-export const REGISTER_PATH = '/register';
-export const FORGOT_PATH = '/forgot';
-export const NEKO_PATH = '/neko';
 export const START_GH_PAGES_PATH = '/neko-login-register-forgot';
+export const SIGN_IN_PATH = START_GH_PAGES_PATH + '/sign-in';
+export const REGISTER_PATH = START_GH_PAGES_PATH + '/register';
+export const FORGOT_PATH = START_GH_PAGES_PATH + '/forgot';
+export const NEKO_PATH = START_GH_PAGES_PATH + '/neko';
 
 const Routes: React.FC = () => {
     return (
         <div>
-            <Route exact path={'/'} render={() => <Redirect to={SIGN_IN_PATH}/>}/>
+            <Route exact path={START_GH_PAGES_PATH} render={() => <Redirect to={SIGN_IN_PATH}/>}/>
 
             <Route path={SIGN_IN_PATH} render={() => <SignInPage/>}/>
             <Route path={REGISTER_PATH} render={() => <RegisterPage/>}/>
             <Route path={FORGOT_PATH} render={() => <ForgotPage/>}/>
 
             <Route path={NEKO_PATH} render={() => <NekoPage/>}/>
-
-            <Route path={START_GH_PAGES_PATH} render={() => <Redirect to={SIGN_IN_PATH}/>}/>
-            {/*// for gh-pages*/}
         </div>
     );
 };
